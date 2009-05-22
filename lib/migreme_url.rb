5fc773f37e0c87209092f6eb854000eeba4143c1
Anderson Dias <andersondaraujo@gmail.com>
2009-05-22 15:23:31 -0300

    Adding encode and decode to migreme_url

--- lib/migreme_url.rb
+++ lib/migreme_url.rb
@@ -1,1 +1,19 @@
-# MigremeUrl
+require 'net/http'
+
+module MigreMeUrl
+  def self.encode(url)
+    xml = get_xml("http://migre.me/api.xml?url=#{url}")
+    xml.match(/migre>(.*)?</)[1]
+  end
+
+  def self.decode(url)
+    xml = get_xml("http://migre.me/api_redirect.xml?url=#{url}")
+    xml.match(/url>(.*)?</)[1]
+  end
+
+  private
+  def self.get_xml(url)
+    uri = URI.parse(url)
+    Net::HTTP.get_response(uri).body
+  end
+end
--- spec/migreme_url_spec.rb
+++ spec/migreme_url_spec.rb
@@ -1,5 +1,55 @@
 require File.dirname(__FILE__) + '/spec_helper'
 describe MigreMeUrl do
-  it 'should return true' do
+  it 'should encode an url an return a migreme url' do
+    url = "http://test/"
+    uri = "http://migre.me/api.xml?url=#{url}"
+    MigreMeUrl.should_receive(:get_xml).with(uri).and_return(
+      <<-XML
+        <item>
+          <title>XML Migre.me</title>
+          <error>0</error>
+          <errormessage>Sem erros</errormessage>
+          <created_at>Fri, 22 May 2009 14:49:32 -0300</created_at>
+          <source>web</source>
+          <date>2009-05-22 14:49:32</date>
+          <id>1oXU</id>
+          <url>http://test/</url>
+          <migre>http://migre.me/1oXU</migre>
+          <category>free</category>
+        </item>
+      XML
+    )
+    MigreMeUrl::encode(url).should == 'http://migre.me/1oXU'
+  end
+
+  it 'should decode an migreme url an return the original url' do
+    url = "http://migre.me/1oXU"
+    uri = "http://migre.me/api_redirect.xml?url=#{url}"
+    MigreMeUrl.should_receive(:get_xml).with(uri).and_return(
+      <<-XML
+        <item>
+          <title>XML Migre.me</title>
+          <error>0</error>
+          <errormessage>No errors</errormessage>
+          <created_at>Fri, 22 May 2009 14:49:05 -0300</created_at>
+          <id>1oXU</id>
+          <migre>http://migre.me/1oXU</migre>
+          <url>http://test/</url>
+        </item>
+      XML
+    )
+    MigreMeUrl::decode(url).should == 'http://test/'
+  end
+
+  describe 'get_xml' do
+  it 'should user Net::HTTP to get response from migreme server' do
+      url = "http://test.com/"
+      uri = URI.parse(url)
+      text_response = 'test'
+      response = mock('Response')
+      response.stub!(:body).and_return(text_response)
+      Net::HTTP.should_receive(:get_response).with(uri).and_return(response)
+      MigreMeUrl.send('get_xml', url).should == text_response
+    end
   end
 end
